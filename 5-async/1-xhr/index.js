6effbe3e5440ff750669e5529ec6c33716cdc86e
chenzdido <chenzdido@gmail.com>
2020-08-10 22:35:47 +0800

    [zhe] feat:complete XMLHttpRequest.

--- 5-async/1-xhr/index.js
+++ 5-async/1-xhr/index.js
@@ -2,7 +2,25 @@ function fetchData(url, successCallback, errorCallback) {
   const xhr = new XMLHttpRequest();
   // <-- start
   // TODO 21: 通过XMLHttpRequest实现异步请求
-
+  //1.create request
+  //1.1 create XMLobject
+  //1.2 set url.method.header
+  xhr.open('POST', url);
+  xhr.setRequestHeader('content-type', 'application/json');
+  //2.register success and failur callback
+  //2.1 register one big callback
+  xhr.onreadystatechange = function() {
+    //2.1.1 register success callback
+    if (xhr.readyState === 4 && xhr.status >= 200 && xhr.readyState <= 299) {
+      successCallback(xhr);
+    }
+    //2.1.2 register failur callback
+    if (xhr.readyState === 4 && (xhr.status < 200 || xhr.readyState > 299)) {
+      errorCallback(xhr);
+    }
+  };
+  //3.send request
+  xhr.send();
   // end -->
 }
 
