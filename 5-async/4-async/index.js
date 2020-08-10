e7420bce7e8b9985b85050c06dd0ffec9e79eab8
chenzdido <chenzdido@gmail.com>
2020-08-10 23:40:08 +0800

    [zhe] feat:modify.

--- 5-async/4-async/index.js
+++ 5-async/4-async/index.js
@@ -1,6 +1,20 @@
 async function fetchData(url) {
   // <-- start
   // TODO 24: 通过await/async实现异步请求
+  await new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', url);
+    xhr.setRequestHeader('content-type', 'application/json');
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status >= 200 && xhr.readyState <= 299) {
+        resolve(xhr);
+      }
+      if (xhr.readyState === 4 && (xhr.status < 200 || xhr.readyState > 299)) {
+        reject(xhr);
+      }
+    };
+    xhr.send();
+  });
   // end -->
 }
 
