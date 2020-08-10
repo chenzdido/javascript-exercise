b266e71d46661908633a69fbb51e49a552a290ad
chenzdido <chenzdido@gmail.com>
2020-08-10 21:17:42 +0800

    [zhe] feat:complete 4-class.

--- 2-array/filter/index.js
+++ 2-array/filter/index.js
@@ -1,6 +1,6 @@
 function chooseMultiplesOfThree(collection) {
   // TODO 1: 在这里写实现代码
-  return collection.filter(num => num % 3 == 0);
+  return collection.filter(num => num % 3 === 0);
 }
 
 function chooseNoRepeatNumber(collection) {
--- 3-object/values/index.js
+++ 3-object/values/index.js
@@ -1,6 +1,6 @@
 export default function countTypesNumber(source) {
   // TODO 6: 在这里写实现代码
-  let num = Object.values(source);
+  const num = Object.values(source);
   let sum = 0;
   num.forEach(item => {
     sum += parseInt(item);
