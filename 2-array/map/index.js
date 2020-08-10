62ae119c1d417c6ec27d5fe9e885805053fc66b0
chenzdido <chenzdido@gmail.com>
2020-08-10 19:45:01 +0800

    [zhe] feat:complete 2-array.

--- 2-array/map/index.js
+++ 2-array/map/index.js
@@ -1,3 +1,7 @@
 export default function numberMapToWord(collection) {
   // TODO 3: 在这里写实现代码
+  const model = ['a', 'b', 'c', 'd', 'e'];
+  return collection.map(num => {
+    return model[num - 1];
+  });
 }
