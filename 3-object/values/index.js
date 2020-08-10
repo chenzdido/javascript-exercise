9164a64152acb39910f0b2a82fcb98fc78bc9f1d
chenzdido <chenzdido@gmail.com>
2020-08-10 20:01:53 +0800

    [zhe] feat:complete 3-object.

--- 3-object/values/index.js
+++ 3-object/values/index.js
@@ -1,3 +1,9 @@
 export default function countTypesNumber(source) {
   // TODO 6: 在这里写实现代码
+  let num = Object.values(source);
+  let sum = 0;
+  num.forEach(item => {
+    sum += parseInt(item);
+  });
+  return sum;
 }
