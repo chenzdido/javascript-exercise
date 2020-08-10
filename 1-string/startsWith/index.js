677bda65c4c1e0fa14fd71be053f4c63e22f75fc
chenzdido <chenzdido@gmail.com>
2020-08-10 15:20:40 +0800

    [zhe] feat:complete 1-string.

--- 1-string/startsWith/index.js
+++ 1-string/startsWith/index.js
@@ -1,3 +1,10 @@
 export default function collectCarNumberCount(collection) {
   // TODO 7: 在这里写实现代码
+  let count = 0;
+  collection.forEach(element => {
+    if (element.slice(0, 2) === '粤A') {
+      count++;
+    }
+  });
+  return count;
 }
